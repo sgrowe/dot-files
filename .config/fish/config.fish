9cef85e003fab194a5b9a9ce6150e32d4dfa77f7
Sam Rowe <sgrowe@live.co.uk>
2019-03-07 15:48:46 +0000

    Set dot_git as a global (not universal) abbreviation to avoid it being stored in fish_variables as well as config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,4 +4,4 @@ set -x VISUAL nano
 
 status is-interactive; and source (nodenv init -|psub)
 
-abbr --add dot_git git --git-dir=$HOME/.cfg/ --work-tree=$HOME
+abbr --add --global dot_git git --git-dir=$HOME/.cfg/ --work-tree=$HOME

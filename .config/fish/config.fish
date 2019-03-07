e326b56cd50ccdc4f2c36b6237bd49e40e79027c
Sam Rowe <sgrowe@live.co.uk>
2019-03-07 15:39:53 +0000

    Update dot_git_init now that dot_git is not a function and avoid hard-coding home folder path in dot_git definition

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,3 +3,5 @@ set -x EDITOR nano
 set -x VISUAL nano
 
 status is-interactive; and source (nodenv init -|psub)
+
+abbr --add dot_git git --git-dir=$HOME/.cfg/ --work-tree=$HOME
--- .config/fish/functions/dot_git_init.fish
+++ .config/fish/functions/dot_git_init.fish
@@ -1,3 +1,3 @@
 function dot_git_init --description "One time set up of git repo for managing dot files"
-    dot_git config --local status.showUntrackedFiles no    
+    git --git-dir=$HOME/.cfg/ --work-tree=$HOME config --local status.showUntrackedFiles no
 end

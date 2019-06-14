7556d22557e358ea9d0a9a16b2bf705474e7ab21
Sam Rowe <sgrowe@live.co.uk>
2019-06-14 16:51:43 +0100

    Only initialise nodenv if it's installed on the PATH

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,7 +2,7 @@
 set -x EDITOR nano
 set -x VISUAL nano
 
-status is-interactive; and source (nodenv init -|psub)
+status is-interactive; and string length (which nodenv) > /dev/null; and source (nodenv init -|psub)
 
 abbr --add --global dot_git git --git-dir=$HOME/.cfg/ --work-tree=$HOME
 
